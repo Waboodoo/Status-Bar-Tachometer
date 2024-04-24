67c232b63f34771b21ebe52c049271863f35564a
Roland Meyer <rmy@rmy.ch>
2024-04-24 17:23:42 +0200

    Updated version number

--- StatusBarSpeedometer/app/build.gradle.kts
+++ StatusBarSpeedometer/app/build.gradle.kts
@@ -11,9 +11,9 @@ android {
         applicationId = "ch.rmy.android.statusbar_tacho"
         minSdk = 21
         targetSdk = 34
-        versionName = "3.8.0"
+        versionName = "3.9.0"
         //noinspection HighAppVersionCode
-        versionCode = 2003080001
+        versionCode = 2003090000
         // 20,(2 digits major),(2 digits minor),(2 digits patch),(2 digits build)
 
         vectorDrawables.useSupportLibrary = true
